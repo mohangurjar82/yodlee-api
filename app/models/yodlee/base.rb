798943d1b534fc22d924f36f3dce926999c316dd
Ryan Morrison <ryanam26@yahoo.com>
2016-09-04 16:47:11 -0400

    yodlee part two complete

--- app/models/yodlee/base.rb
+++ app/models/yodlee/base.rb
@@ -14,6 +14,8 @@ module Yodlee
       response = self.class.send(method, opts[:endpoint], query: opts[:params])
       data     = response.parsed_response
 
+      log_query(opts.merge({response: data, code: response.code}))
+
       if response.success?
         if [ TrueClass, FalseClass, Fixnum ].include?(data.class)
           data
@@ -52,6 +54,16 @@ module Yodlee
 
     def fresh_token?
       current_session_token && current_session_started && current_session_started >= 90.minutes.ago
+    end
+
+    def log_query opts
+    Log.create!(
+    :method        => opts[:method],
+    :endpoint      => opts[:endpoint],
+    :params        => opts[:params].to_json,
+    :response      => opts[:response].to_json,
+    :response_code => opts[:code]
+    )
     end
 
   end
